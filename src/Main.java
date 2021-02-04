41b9312627ef266fa7af9ff483fd05a61c95aafc
Gaston Deseine <gaston.deseine@ig2i.centralelille.fr>
2021-02-04 10:57:09 +0100

    debut menu

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-import modele.UtilisateurEntity;
 import org.hibernate.HibernateException;
-import org.hibernate.Metamodel;
-import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.metamodel.EntityType;
-
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main
 {
@@ -35,7 +32,8 @@ public class Main
 
     public static void main(final String[] args) throws Exception
     {
-        final Session session = getSession();
+        menu();
+        /*final Session session = getSession();
         try
         {
             System.out.println("Select utilisateurs");
@@ -70,7 +68,7 @@ public class Main
                 System.out.println("  " + utilisateur.getPseudo());
             }
 
-/*
+*//*
             System.out.println("querying all the managed entities...");
             final Metamodel metamodel = session.getSessionFactory().getMetamodel();
             for (EntityType<?> entityType : metamodel.getEntities())
@@ -82,10 +80,75 @@ public class Main
                 {
                     System.out.println("  " + o);
                 }
-            }*/
+            }*//*
         } finally
         {
             session.close();
+        }*/
+    }
+
+
+    public static void menu() throws IOException
+    {
+        System.out.println("Selectionnez en entrant le numéro du menu");
+        System.out.println("-----------------------------------------");
+        System.out.println("1 - Gestion des caches");
+        System.out.println("2 - Gestion des utilisateurs");
+        System.out.println("3 - Gestion des lieux");
+        System.out.println("4 - Gestion des visites");
+        //Enter data using BufferReader
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String res = reader.readLine();
+
+        System.out.println("-----------------------------------------");
+
+        switch (Integer.parseInt(res)){
+            case 1:
+                menu1();
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            case 4:
+                System.out.println(4);
+                break;
+        }
+    }
+
+
+    public static void menu1() throws IOException
+    {
+        System.out.println("-----------------------------------------");
+        System.out.println("1 - Ajouter une cache");
+        System.out.println("2 - Modifier une cache");
+        System.out.println("3 - Supprimer une cache");
+        System.out.println("4 - Rechercher une cache");
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String res = reader.readLine();
+
+        System.out.println("-----------------------------------------");
+
+        switch (Integer.parseInt(res)){
+            case 1:
+                System.out.println(11);
+                break;
+            case 2:
+                System.out.println(12);
+                break;
+            case 3:
+                System.out.println(13);
+                break;
+            case 4:
+                System.out.println(14);
+                break;
         }
     }
 }
